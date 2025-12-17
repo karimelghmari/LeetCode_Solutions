7b2a6c3a899f9ad6d1c4acb240da46678b8f1874
karimelghmari <karimelghmari1e@gmail.com>
2025-12-17 20:12:16 +0100

    Time: 0 ms (100%), Space: 10.6 MB (96.64%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -6,19 +6,19 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-   if(list1==NULL)return list2;
-   if(list2==NULL)return list1;
-   struct ListNode*list3=NULL;
-   if(list1->val<=list2->val){
-    list3=list1;
-    list1=list1->next;
+    if(list1==NULL)return list2;
+    if(list2==NULL)return list1;
+    struct ListNode*list3=NULL;
+    if(list1->val<=list2->val){
+        list3=list1;
+        list1=list1->next;
    }
-   else{
+    else{
     list3=list2;
     list2=list2->next;
    }
-   struct ListNode*p=list3;
-   while(list1!=NULL && list2!=NULL){
+    struct ListNode*p=list3;
+    while(list1!=NULL && list2!=NULL){
     if(list1->val<=list2->val){
         p->next=list1;
         list1=list1->next;
@@ -29,7 +29,8 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     }
     p=p->next;
    }
-   if(list1!=NULL)p->next=list1;
-   else p->next=list2;
-   return list3;
+    if(list1!=NULL)p->next=list1;
+    else p->next=list2;
+    return list3;
+    
 }
